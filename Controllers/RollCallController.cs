0bf22051f5e76e98f69174a0748e4f56d58dae4d
Vicente Estrada <201112007@alumnos.utn.edu.mx>
2021-09-09 18:50:38 -0500

    Agregado de nuevas tablas

---
+++ Controllers/RollCallController.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAPITecsa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RollCallController : ControllerBase
+    {
+        private string _connection = @"Server = localhost; Database = tecsaoffice; Uid=root; Pwd = 12345678";
+        [HttpGet]
+        public IActionResult GetRollCall()
+        {
+            try
+            {
+                IEnumerable<Models.RollCall> lst = null;
+                using (var db = new MySqlConnection(_connection))
+                {
+                    var sql = "SELECT id_rol,name FROM rol";
+
+                    lst = db.Query<Models.RollCall>(sql);
+                }
+
+                return Ok(lst);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult InsertRollCall(Models.RollCall model)
+        {
+            try
+            {
+                int result = 0;
+                using (var db = new MySqlConnection(_connection))
+                {
+                    var sql = "INSERT INTO rol(name)" +
+                        " VALUES(@name)";
+                    result = db.Execute(sql, model);
+                }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/RollCall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAPITecsa.Models
+{
+    public class RollCall
+    {
+        public int Id_day { get; set; }
+        public string Name_day { get; set; }
+        public int Id_user { get; set; }
+    }
+}
